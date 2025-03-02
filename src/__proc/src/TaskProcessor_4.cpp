b72d2ef6b02f2f832ce3332962eadaf9f26407e9
Alex Nesov <magicofasword@gmail.com>
2025-03-02 22:08:42 +0200

    Tests added for TaskProcessors

--- src/__proc/src/TaskProcessor_4.cpp
+++ src/__proc/src/TaskProcessor_4.cpp
@@ -5,7 +5,7 @@ TaskProcessor_4::TaskProcessor_4():m_parser(kPatternForTask2){}
 std::string TaskProcessor_4::execute(const std::string& data) {
      m_parser.parse(data);
      std::vector<std::string> vec = m_parser.getParseResults();
-     if (vec.size() < 2 ) return "Array is 2 and more elements \n";
+     if (vec.size() < 2 ) return "Array is 2 and more elements.";
      
      std::vector<double> doubles;
      for (const std::string& str : vec) {
